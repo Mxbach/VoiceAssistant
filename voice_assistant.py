478bba50e973c14519d24dc3fa934ba9b6935219
Max A. <maximiliana28@gmail.com>
2025-01-30 02:28:37 +0100

    added chatgpt connection + minor changes
    - added venv to .gitignore
    - transcribed text is sent to chatgpt through openai api
    - response TTS with openaiTTS
    - playing of response

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,11 +1,19 @@
 import whisper
 import speech_recognition as sr
 import tempfile
+from openai import OpenAI
+import os
+import sys
+from dotenv import load_dotenv
+from playsound import playsound
 
 class VoiceAssistant:
     def __init__(self, model_size: str="turbo"):
+        load_dotenv()
         self.model = whisper.load_model(model_size)
         self.recognizer = sr.Recognizer()
+        self.client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+        self.context = [{"role": "system", "content": "You are an AI Assistant that helps people find information. Keep your answers really concise"}]
 
     def listen(self) -> str:
         with sr.Microphone() as source:
@@ -27,13 +35,37 @@ class VoiceAssistant:
             return None
 
         return result["text"]
+    
+    def sent_gpt_request(self, prompt: str) -> str:
+        self.context.append({"role": "user", "content": prompt})
+        completion = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=self.context
+        )
+        response = completion.choices[0].message.content
+        self.context.append({"role": "assistant", "content": response})
+        return response
 
-    def run_assistant(self):
-        
+    def text_to_speech(self, text):
+        response = self.client.audio.speech.create(
+            model="tts-1",
+            voice="nova",
+            input=text,
+        )
+        response.stream_to_file("output.mp3")
+
+    def run_assistant(self): 
         audio_file = self.listen()
         text = self.audio_to_text(audio_file)
-        print(text)
+        os.remove(audio_file)
+        
+        print(f"You said: {text}")
+        response = self.sent_gpt_request(text)
+        print(f"ChatGPT: {response}")
+        self.text_to_speech(response) # audio file
+        playsound("output.mp3")
+        sys.exit(0)
 
 if __name__ == "__main__":
-    jarvis = VoiceAssistant()
-    jarvis.run_assistant()
+    va = VoiceAssistant()
+    va.run_assistant()
